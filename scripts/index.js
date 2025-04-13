a122cdd67a957deeda49cb84862219b782f21bd4
usmanz0 <usmanrizwanshamshi125@gmail.com>
2025-04-13 17:33:36 +0500

    question  card done

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,14 @@ const yearsContinueElement = document.getElementById('yearsContinue')
 const yearsError = document.getElementById('yearsError');
 const questionNumber = document.getElementById('cardNumber');
 const addGoalsElement = document.getElementById('addGoals');
+const overlay = document.querySelector('.overlay');
+const addGoalsModalElement = document.getElementById('addGoalsModal');
+const goalsContinueElement = document.getElementById('goalsContinue');
 let yearsValue;
 let currentCard = 1;
 const goalsList = [];
+
+questionNumber.innerHTML = `${currentCard}/2`
 
 rangeInput.addEventListener('input', () => {
   rangeValue.style.color = 'black';
@@ -32,21 +37,42 @@ yearsContinueElement.addEventListener('click', () => {
 })
 
 addGoalsElement.addEventListener('click', () => {
-  if (goalsList.length === 0) {
-    document.getElementById('goalsList').innerHTML = 'Error';
-  } else {
-    addGoals();
-  }
+  // if (goalsList.length === 0) {
+  //   document.getElementById('goalsError').innerHTML = 'Max Limit Reached';
+  //   setTimeout(() => {
+  //     document.getElementById('goalsError').innerHTML = '';
+  //   },1500)
+  // } else {
+  // }
+
+  const modal = document.querySelector('.goals-modal');
+  overlay.classList.remove('hidden')
+
+  modal.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+  overlay.addEventListener('click', () => {
+    overlay.classList.add('hidden')
+  })
 });
 
-function addGoals() {
-  
+
+addGoalsModalElement.addEventListener('click', () => {
+  const inputGoalsModal = document.getElementById('inputGoalsModal');
+  addGoals(inputGoalsModal.value);
+
+  overlay.classList.add('hidden')
+})
+
+function addGoals(goal) {
+  goalsList.push(goal);
+  renderGoalsList();
 };
 
 function renderGoalsList () {
-  let goalsHTML;
-  goalsList.forEach((goalsObject,index) => {
-    goalsHTML += goalsObject;
+  let goalsHTML = '';
+  goalsList.forEach((goalsObject) => {
+    goalsHTML += `<li class="paragraph-text" id="goals">${goalsObject}</li>`;
   })
   document.getElementById('goalsList').innerHTML = goalsHTML;
 };
@@ -62,8 +88,15 @@ function nextCard() {
     currentCardElement.classList.remove('fade-out');
 
     nextCardElement.classList.remove('hidden');
+    nextCardElement.classList.remove('fade-out');
     nextCardElement.classList.add('fade-in');
 
     currentCard++;
+    questionNumber.innerHTML = `${currentCard}/2`
   }, 500);
 };
+
+goalsContinueElement.addEventListener('click', () => {
+  document.getElementById('questionCard').
+    classList.add('fade-out')
+})
