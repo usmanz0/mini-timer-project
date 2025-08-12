00812f47c6213e4abf5328bcf2a1271120c84ddc
usmanz0 <usmanrizwanshamshi125@gmail.com>
2025-08-12 20:39:58 +0500

    darkmode , pomodoro setting fixed and functional

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,9 @@ import { renderPomodoro } from "./pomodoro.js";
 import { renderUserOnBoard } from "./userOnboard.js";
 import { renderTasksList , renderGoalsList} from "../data/goalsAndTasks.js";
 
-
-
 if (localStorage.getItem('isDarkModeToggled') === 'true') {
   document.body.classList.add('dark-mode');
-    ['p', 'strong', 'small' , 'h1', '.main-heading', '.content-heading', '.timer', '.countdown-timer-section', '.settings-image'].forEach(selector => {
+    ['p', 'strong', 'small' , 'h1', '.main-heading', '.content-heading', '.timer', '.countdown-timer-section', '.settings-image', '.timer-button'].forEach(selector => {
     document.querySelectorAll(selector).forEach(el => el.classList.add('dark-mode'));
   });
 }
--- scripts/settings.js
+++ scripts/settings.js
@@ -12,8 +12,8 @@ const timerInputs = {
   longBreakInputEl : document.getElementById('longBreakInput')
 }
 const countdownYearsInputEl = document.getElementById('changeYearsInput');
-const initialStates = {
-  isDarkModeToggled: localStorage.getItem('isDarkModeToggled') || 'true',
+const savedSettings = {
+  isDarkModeToggled: localStorage.getItem('isDarkModeToggled'),
   isGoalVisibleToggled: localStorage.getItem('isGoalVisibleToggled'),
   isTaskVisibleToggled: localStorage.getItem('isTaskVisibleToggled'),
   isPomodoroVisibleToggled: localStorage.getItem('isPomodoroVisibleToggled'),
@@ -24,80 +24,57 @@ const initialStates = {
   longBreak: JSON.parse(localStorage.getItem('longBreak'))
 };
 
-// CHECKS BUTTON TOGGLE STATE
+checkDarkMode();
 checkToggleBtn();
-
-// TOGGLE BUTTON STATE CHANGER
-toggleEvent(darkModeEl,'isDarkModeToggled');
-toggleEvent(goalVisibleEl,'isGoalVisibleToggled');
-toggleEvent(taskVisibleEl,'isTaskVisibleToggled');
-toggleEvent(pomodoroVisibleEl,'isPomodoroVisibleToggled');
-toggleEvent(remCompGoalEl,'isRemCompGoalToggled');
-toggleEvent(remCompTaskEl,'isRemCompTaskToggled');
-
-// APPLIES SETTING 
-applySetting();
-
-// UPDATE THE PLACEHOLDER OF THE SETTINGS INPUT
 updateInputPlaceholder();
+applySetting();
 
 function applySetting() {
   applyButtonEl.addEventListener('click', () => {
-    checkDarkMode();
-    if (initialStates.focus !== timerInputs.focusInputEl.value) {
+    if (darkModeEl.checked) {
+      localStorage.setItem('isDarkModeToggled', 'true')
+      checkDarkMode()
+    } else {
+      localStorage.setItem('isDarkModeToggled', 'false')
+      checkDarkMode()
+    }
+    if (savedSettings.focus !== timerInputs.focusInputEl.placeholder) {
       pomodoroTimerInput(timerInputs.focusInputEl);
-    } if (initialStates.shortBreak !== timerInputs.shortBreakInputEl.value) {
+    }
+    if (savedSettings.shortBreak !== timerInputs.shortBreakInputEl.placeholder) {
       pomodoroTimerInput(timerInputs.shortBreakInputEl);
-    } if (initialStates.longBreak !== timerInputs.longBreakInputEl.value) {
+    }
+    if (savedSettings.longBreak !== timerInputs.longBreakInputEl.placeholder) {
       pomodoroTimerInput(timerInputs.longBreakInputEl);
     }
+    if (remCompTaskEl.checked) {
+      localStorage.setItem('isRemCompTaskToggled','true')
+    } else {
+      localStorage.setItem('isRemCompTaskToggled','false')
+    }
+    if (remCompGoalEl.checked) {
+      localStorage.setItem('isRemCompGoalToggled','true')
+    } else {
+      localStorage.setItem('isRemCompGoalToggled','false')
+    }
   })
-  checkDarkMode();
 }
 
 function updateInputPlaceholder() {
-  if (initialStates.focus === '') {
+  if (savedSettings.focus === '') {
     timerInputs.focusInputEl.placeholder = '00';
   } else {
-    timerInputs.focusInputEl.placeholder = initialStates.focus;
+    timerInputs.focusInputEl.placeholder = savedSettings.focus;
   }
-  if (initialStates.shortBreak === '') {
+  if (savedSettings.shortBreak === '') {
     timerInputs.shortBreakInputEl.placeholder = '00';
   } else {
-    timerInputs.shortBreakInputEl.placeholder = initialStates.shortBreak;
+    timerInputs.shortBreakInputEl.placeholder = savedSettings.shortBreak;
   }
-  if (initialStates.longBreak === '') {
+  if (savedSettings.longBreak === '') {
     timerInputs.longBreakInputEl.placeholder = '00';
   } else {
-    timerInputs.longBreakInputEl.placeholder = initialStates.longBreak;
-  }
-}
-
-export function checkDarkMode() {
-  if (localStorage.getItem('isDarkModeToggled') === 'true') {
-    document.body.classList.add('dark-mode');
-    ['p', 'strong', 'small' , 'h1', '.content-heading', '.main-heading'].forEach(selector => {
-    document.querySelectorAll(selector).forEach(el => el.classList.add('dark-mode'));
-    });
-
-  } else {
-    document.body.classList.remove('dark-mode');
-    ['p', 'strong', 'small' , 'h1', '.main-heading'].forEach(selector => {
-    document.querySelectorAll(selector).forEach(el => el.classList.remove('dark-mode'));
-    });
-  }
-}
-
-function toggleEvent(toggleEl,toggleState) {
-  toggleEl.addEventListener('change', () => {
-    checkToggle(toggleEl,toggleState)
-  })
-}
-function checkToggle(button,stateButton) {
-  if (button.checked) {
-    localStorage.setItem(`${stateButton}`,'true');
-  } else {
-    localStorage.setItem(`${stateButton}`,'false');
+    timerInputs.longBreakInputEl.placeholder = savedSettings.longBreak;
   }
 }
 
@@ -122,8 +99,24 @@ function checkToggleBtn() {
   }
 }
 
+export function checkDarkMode() {
+  if (localStorage.getItem('isDarkModeToggled') === 'true') {
+    document.body.classList.add('dark-mode');
+    ['p', 'strong', 'small' , 'h1', '.content-heading', '.main-heading'].forEach(selector => {
+    document.querySelectorAll(selector).forEach(el => el.classList.add('dark-mode'));
+    });
+
+  } else {
+    document.body.classList.remove('dark-mode');
+    ['p', 'strong', 'small' , 'h1', '.main-heading'].forEach(selector => {
+    document.querySelectorAll(selector).forEach(el => el.classList.remove('dark-mode'));
+    });
+  }
+}
+
 function pomodoroTimerInput(input) {
-  const inputValue = input.value
+  const inputValue = input.value;
   const inputId = input.id.slice(0, -5);
+
   localStorage.setItem(`${inputId}`, JSON.stringify(inputValue));
 }
