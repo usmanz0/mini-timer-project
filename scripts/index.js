d2d4f14aecbe625c3869cde684c64792f5ed4fbb
usmanz0 <usmanrizwanshamshi125@gmail.com>
2025-08-17 16:07:19 +0500

    change file name

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 import { renderPomodoro } from "./indexUtils/pomodoro.js";
 import { renderUserOnBoard } from "./indexUtils/userOnboard.js";
 import { renderTasksList , renderGoalsList, goalsList, tasksList} from "../data/goalsAndTasks.js";
-import { checkDarkMode } from "./indexUtils/checkDarkMode.js"
+import { checkDarkMode } from "./indexUtils/checkIndexDarkMode.js"
 
 
 renderUserOnBoard()
--- scripts/settings.js
+++ scripts/settings.js
@@ -1,4 +1,4 @@
-import { checkDarkMode } from "./settingsUtils/checkDarkMode.js";
+import { checkDarkMode } from "./settingsUtils/checkSettingsDarkMode.js";
 import { checkToggleBtn } from "./settingsUtils/checkToggles.js";
 import { updateInputPlaceholder } from "./settingsUtils/UpdateInputButton.js";
 import { applySetting } from "./settingsUtils/saveSettings.js";
